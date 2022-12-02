e71224abb5b1ec1a28090ef2c0a9c743b470913d
sgalati528 <59853812+sgalati528@users.noreply.github.com>
2022-12-02 14:11:40 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,3 @@
+window.onload = setTimeout(function(){
+    alert('Sorry, it seems we found another candidate who was better fit for the job. We encourage you to apply for another position.');
+}, 20000);
---
+++ java1.js
@@ -0,0 +1,3 @@
+window.onload = setTimeout(function(){
+    alert("Welcome to the team! You're hired! Your first shift starts tomorrow at 4:00AM :)");
+}, 20000);
